b7980b299002ce319885115e4671c9b3b7a332c1
大石航平 <oishikohei@ooishikouheis-MacBook-Pro.local>
2025-05-22 10:02:58 +0100

    add cors header

--- api/check-website.js
+++ api/check-website.js
@@ -6,10 +6,10 @@ module.exports = async (req, res) => {
   // Set CORS headers
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,POST,PUT,DELETE");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
   );
 
   // Handle preflight request
